6c148f5d9f7ddc6a3f290278e3ecb26600b1aafa
Austin New <literalsands@gmail.com>
2017-04-20 00:01:20 -0400

    fixed file path. adde node user as a system user.

--- provision_node_user.sh
+++ provision_node_user.sh
@@ -2,17 +2,18 @@
 
 # Make a new user and group, node.
 getent group node || groupadd node
-id -u node &>/dev/null || useradd node -g node
-# Put the current user in the node group.
-usermod -aG node austin
+id -u node &>/dev/null || useradd node -g node -r
+
 # Own node application folder.
 mkdir -p /var/opt/node
 chown -R node:node /var/opt/node
 chmod -R 775 /var/opt/
+
 # Own global NPM modules.
-mkdir -p /var/local/lib/node_modules
+mkdir -p /usr/local/lib/node_modules
 chown -R node:node /usr/local/lib/node_modules
 chmod -R 775 /usr/local/lib/node_modules
+
 # Own NVM folder.
 mkdir -p /usr/local/nvm
 chown -R node:node /usr/local/nvm
