a97291b3da45a33408c4a4f5b9c375d29d9114fb
Austin New <literalsands@gmail.com>
2017-05-03 08:30:42 -0400

    updates provisioning actions.

--- letsencrypt-nginx.sh
+++ letsencrypt-nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Check that nginx, letsencrypt, and openssl are installed and up to date.
-rsync -au /etc/nginx/snippets/ $REMOTE:/etc/nginx/
-rsync -au /etc/nginx/sites-available/ $REMOTE:/etc/nginx/
-./remote.sh $REMOTE -f provision_nginx.sh
+rsync -au /etc/nginx/snippets/ $REMOTE:/etc/nginx/snippets
+rsync -au /etc/nginx/sites-available/ $REMOTE:/etc/nginx/sites-available
+./remote.sh $REMOTE -f provision_nginx.sh > /dev/null
 # Copy over letsencrypt certificates if they've already been created.
 # Else run letsencrypt.
 ./remote.sh $REMOTE -f provision_nginx_letsencrypt.sh -e "DOMAIN=$DOMAIN EMAIL=$EMAIL"
--- provision_nginx_ufw.sh
+++ provision_nginx_ufw.sh
@@ -3,4 +3,4 @@ ufw allow "Nginx Full"
 ufw allow 22
 ufw allow 80
 ufw allow 443
-ufw enable
+ufw --force enable
